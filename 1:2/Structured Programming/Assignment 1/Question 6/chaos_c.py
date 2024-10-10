c48f2faa128257939b7a5d2e7675ab3e6df5b0bb
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-10 23:33:23 +0300

    Create  chaos_c.py

---
+++ 1:2/Structured Programming/Assignment 1/Question 6/chaos_c.py
@@ -0,0 +1,18 @@
+#QUESTION: The calculation performed in the chaos program can be written 
+#in a number of ways that are algebraically equivalent. 
+#Write a version of the chaos program for each of the following ways 
+#of doing the computation. Have your modified programs print out 100 
+#iterations of the function and compare the results when run on the same input.
+
+
+#(c) 3.9 * x - 3.9 * x * x
+
+def main():
+    print("This program illustrates a chaotic function")
+    x = eval(input("Enter a number between 0 and 1: "))
+    for i in range(100):
+        x = 3.9*x-3.9*x*x
+        print(x)
+        
+main()
+
