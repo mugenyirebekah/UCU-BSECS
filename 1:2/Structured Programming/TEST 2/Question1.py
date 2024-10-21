7eba5d8f7114880596a3bc113625de65c1f81e07
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-21 11:42:04 +0300

    Create Question1.py

---
+++ 1:2/Structured Programming/TEST 2/Question1.py
@@ -0,0 +1,7 @@
+#Question 1: Write a Python program that takes a list of integers as input and returns a list of even numbers.
+
+integers_list = [1,2,3,4,5,6,7,8,9,10]
+
+for i in integers_list:
+    if(i%2==0):
+        print(i)
