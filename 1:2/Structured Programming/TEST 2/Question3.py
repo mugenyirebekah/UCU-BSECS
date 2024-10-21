254a1d11fca1da2aefeb20870b9bddf982b0b385
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-21 11:44:31 +0300

    Create Question3.py

---
+++ 1:2/Structured Programming/TEST 2/Question3.py
@@ -0,0 +1,11 @@
+#Question 3: Write a Python program that finds the largest number in a list without using the max() function.
+
+numbers = [1,7,3,9,4,8,13,124,57]
+
+largest = 0
+
+for i in numbers:
+    if i > largest:
+        largest = i
+
+print("The largest number is", largest)
