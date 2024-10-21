4dac4462277c7fcf8f4b10a0c39f7aa196483a14
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-21 11:45:32 +0300

    Create Question4.py

---
+++ 1:2/Structured Programming/TEST 2/Question4.py
@@ -0,0 +1,12 @@
+
+#Question 4: Write a Python function that checks if a string is the same as the input String
+
+string = "Structured Programming"
+input_string = input("Print the phrase: Structured Programming: ")
+
+
+if string == input_string:
+    print("The string is the same as the input string")
+
+else:
+    print("The string is not the same as the input string")
