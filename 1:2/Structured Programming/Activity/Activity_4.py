3cc54f906766dc42bebe14b1f75422fbb33b3f6a
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-31 15:04:21 +0300

    Create Activity_4.py

---
+++ 1:2/Structured Programming/Activity/Activity_4.py
@@ -0,0 +1,18 @@
+'''
+ACTIVITY
+
+1. Write a Python function that checks if a string is the same as the input String
+2. Write a Python program to print the multiplication table of a given number up to 12.
+3. Write a Python program that finds the largest number in a list without using the max() function.
+4. Write a Python program that takes a list of integers as input and returns a list of even numbers.
+'''
+
+#ACTIVITY 4
+
+list = [1,2,3,4,5,6,7,8,9,10]
+
+largest = list[0]
+
+for i in list:
+    if i%2==0:
+        print(i)
